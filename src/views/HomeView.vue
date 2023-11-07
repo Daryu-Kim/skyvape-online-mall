fda7be3f3dbfad167ebeac661152c2d0f8a14c46
Won Jae Kim <86081049+Daryu-Kim@users.noreply.github.com>
2023-11-07 09:47:16 +0900

    2023-11-06 04:17 Update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,80 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination, Navigation, EffectCoverflow, Autoplay } from "swiper/modules";
+
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/effect-coverflow';
+import router from "@/router";
+
+const modules = [Pagination, Navigation, EffectCoverflow, Autoplay];
+const swiper_urls = [
+  {
+    url: "https://picsum.photos/1000",
+    route: "/123"
+  },
+  {
+    url: "https://picsum.photos/1001",
+    route: "/1234"
+  },
+  {
+    url: "https://picsum.photos/1002",
+    route: "/12345"
+  },
+];
+
+function bannerClick(link: string) {
+  router.push(link)
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Swiper
+        class="skyvape-main-banner"
+        :autoplay="{
+          delay: 5000,
+          disableOnInteraction: false,
+        }"
+        :slidesPerView="'auto'"
+        :pagination="{
+          clickable: true,
+        }"
+        :navigation="true"
+        :modules="modules"
+        :loop="true"
+        :grabCursor="true"
+        :centeredSlides="true"
+        :effect="'coverflow'"
+        :coverflowEffect="{
+          rotate: 45,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }"
+    >
+      <SwiperSlide
+          class="skyvape-main-banner-slide"
+          v-for="(item, index) in swiper_urls"
+          :key="index"
+          :style="`background-image: url(${item.url})`"
+          @click="bannerClick(item.route)"
+      />
+    </Swiper>
   </main>
 </template>
 
 <style scoped lang="scss">
-main {
-  min-height: 100vh;
+.skyvape-main-banner {
+  aspect-ratio: 3 / 1.25;
+
+  > .skyvape-main-banner-slide {
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
 }
 </style>
---
+++ src/views/PaymentView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import * as PortOne from "@portone/browser-sdk/v2";
+async function adultRequest() {
+  const response = await PortOne.requestIdentityVerification({
+    storeId: "store-ea6b352b-061d-4dec-872c-05067992a259",
+    identityVerificationId: "q34r-qn93r8mw09e09x",
+    channelKey: "channel-key-98690180-8c66-4b18-978d-85fb106ddaca",
+  });
+  console.log(response);
+}
+
+function paymentRequest() {
+  PortOne.requestPayment({
+    // 가맹점 storeId로 변경해주세요.
+    storeId: 'store-ea6b352b-061d-4dec-872c-05067992a259',
+    paymentId: 'paymentId_{now()}',
+    orderName: '나이키 와플 트레이너 2 SD',
+    totalAmount: 1000,
+    currency: 'CURRENCY_KRW',
+    pgProvider: 'PG_PROVIDER_TOSSPAYMENTS',
+    payMethod: "CARD"
+  });
+}
+</script>
+
+<template>
+  <main>
+    <button @click="adultRequest">본인인증</button>
+    <button @click="paymentRequest">결제테스트</button>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.skyvape-main-banner {
+  aspect-ratio: 3 / 1.25;
+
+  > .skyvape-main-banner-slide {
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+}
+</style>
